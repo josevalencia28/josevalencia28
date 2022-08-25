59a7df18953ff37cd7a762997fecd26254290082
josevalencia28 <joseluisvalencia654@gmail.com>
2022-08-25 17:34:36 -0500

    update

--- app.js
+++ app.js
@@ -1,4 +1,16 @@
 const express = require('express');
 const app = express();
 
+
+// settings
+
+//middlewares
+
+//routes
+
+//static files
+
+//start the server
+
+
 app.listen(3000, () => console.log('server on port 3000'));
