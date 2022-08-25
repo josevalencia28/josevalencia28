ee7cf0ebb3b6d3462cc4fbf34149be435bdc6778
josevalencia28 <joseluisvalencia654@gmail.com>
2022-08-25 17:54:41 -0500

    update

--- app.js
+++ app.js
@@ -1,11 +1,17 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 
 
 // settings
+app.set('port', process.env.PORT || 3000);
+app.set('views', path.join(__dirname, 'views'));
+app.set('views', 'ejs');
 
 //middlewares
-
+app.use((req,res,next) => {
+    console.log('')
+})
 //routes
 
 //static files
@@ -13,4 +19,6 @@ const app = express();
 //start the server
 
 
-app.listen(3000, () => console.log('server on port 3000'));
+app.listen(app.get('port'), () => {
+    console.log('server on port', app.get('port'));
+});
