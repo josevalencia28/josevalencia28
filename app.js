1dc25932af8585d3e23c2e72a8fce9b3c9a7b86e
josevalencia28 <joseluisvalencia654@gmail.com>
2022-08-26 14:24:49 -0500

    update

--- app.js
+++ app.js
@@ -9,20 +9,18 @@ const routes = require('./routes/index.js');
 // settings
 app.set('port', process.env.PORT || 3000);
 app.set('views', path.join(__dirname, 'views'));
-app.set('views', 'ejs');
+app.set('view engine', 'ejs');
 
 //middlewares
 app.use((req,res,next) => {
     console.log(`${req.url} -${req.method}`);
     next();
 })
-
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended:false }));
 
 //routes
 app.use(routes);
-
 //static files
 app.use(express.static(path.join(__dirname, 'public')));
 
