9503e7e66bf7bb1b2aa0d58c9523b9d36eb191d6
josevalencia28 <joseluisvalencia654@gmail.com>
2022-08-26 16:39:57 -0500

    update

--- routes/index.js
+++ routes/index.js
@@ -2,9 +2,15 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-    res.render('index', {
+    res.render('index', { 
         title: 'My WebStore'
-    });
+    })
+});
+
+router.get('/products', (req, res, next ) => {
+  res.render('products', {
+    title: 'List of products'
+  });;
 });
 
 module.exports = router;
