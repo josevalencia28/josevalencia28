742411b24e43b8f75999bd9c2601bd3ac60f2723
josevalencia28 <joseluisvalencia654@gmail.com>
2022-08-26 10:29:08 -0500

    update

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const path = require('path');
+const bodyParser = require('body-parser');
 const app = express();
 
 const routes = require('./routes/index.js');
@@ -15,9 +16,13 @@ app.use((req,res,next) => {
     console.log(`${req.url} -${req.method}`);
     next();
 })
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
 //routes
 app.use(routes);
 //static files
+app.use(express.static(path.join(__dirname, 'public')));
 
 //start the server
 
