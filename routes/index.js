5373bd0f74bc16743ee46b5c56391cc5f2aac275
josevalencia28 <joseluisvalencia654@gmail.com>
2022-08-26 10:38:29 -0500

    update

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-    res.end('Hello, world!');
+    res.render('index')
 });
 
 module.exports = router;
