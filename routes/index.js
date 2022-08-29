229f2c49a1d51c56f41640fc16904a099311f006
josevalencia28 <joseluisvalencia654@gmail.com>
2022-08-29 15:52:56 -0500

    UPDATE

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,12 @@
 const express = require('express');
 const router = express.Router();
 
+const items = [
+  {id: 1, name: 'productos1'},
+  {id: 2, name: 'productos2'},
+  {id: 3, name: 'productos3'}
+];
+
 router.get('/', (req, res) => {
     res.render('index', { 
         title: 'My WebStore'
@@ -9,10 +15,12 @@ router.get('/', (req, res) => {
 
 router.get('/products', (req, res, next ) => {
   res.render('products', {
-    title: 'Lista de Productos'
-  })
+    title: 'Lista de Productos',
+    items: items
+  });
 
 });
+
 
 module.exports = router;
 
